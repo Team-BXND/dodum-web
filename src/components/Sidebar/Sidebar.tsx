ffd1e2360c84a20987717bb0cbec0f9802493900
chaeyn <chaulfe@gmail.com>
2025-09-22 21:15:01 +0900

    refactor: 상수 데이터 분리, 사이드바 탭 통일

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,31 +1,6 @@
 import * as S from './Sidebar.style.ts';
-import InfoShareIcon from '@/assets/sidebar/info-share.svg';
-import ArchiveIcon from '@/assets/sidebar/archive.svg';
-import MajorSelectIcon from '@/assets/sidebar/major-select.svg';
-import ContestInfoIcon from '@/assets/sidebar/contest-info.svg';
-import EtcInfoIcon from '@/assets/sidebar/etc-info.svg';
-import ProfileIcon from '@/assets/sidebar/profile.svg';
+import { tabList } from '@/constants/sidebar.constants.tsx';
 import { useLocation } from 'react-router';
-
-interface TabItem {
-  id: string;
-  name: string;
-  icon: React.ReactElement;
-  to: string;
-}
-
-const tabList: TabItem[] = [
-  { id: 'info', name: '정보공유', icon: <InfoShareIcon />, to: '/info' },
-  { id: 'archive', name: '아카이브', icon: <ArchiveIcon />, to: '/archive' },
-  { id: 'major', name: '전공선택', icon: <MajorSelectIcon />, to: '/major' },
-  {
-    id: 'contest',
-    name: '대회정보',
-    icon: <ContestInfoIcon />,
-    to: '/contest',
-  },
-  { id: 'etc', name: '기타정보', icon: <EtcInfoIcon />, to: '/etc' },
-];
 
 export default function Sidebar() {
   const location = useLocation();
@@ -33,28 +8,31 @@ export default function Sidebar() {
     <S.Container>
       <S.Logo />
       <S.Tabs>
-        {tabList.map((item) => (
+        {tabList
+          .filter((item) => !item.isBottom)
+          .map((item) => (
+            <S.Tab
+              key={item.id}
+              to={item.to}
+              $active={location.pathname.startsWith(item.to)}
+            >
+              {item.icon}
+              {item.name}
+            </S.Tab>
+          ))}
+      </S.Tabs>
+      {tabList
+        .filter((item) => item.isBottom)
+        .map((item) => (
           <S.Tab
             key={item.id}
             to={item.to}
-            $active={
-              item.to === '/'
-                ? location.pathname === '/'
-                : location.pathname.startsWith(item.to)
-            }
+            $active={location.pathname.startsWith(item.to)}
           >
             {item.icon}
             {item.name}
           </S.Tab>
         ))}
-      </S.Tabs>
-      <S.Tab
-        key="profile"
-        to="/profile"
-        $active={location.pathname.startsWith('/profile')}
-      >
-        <ProfileIcon />내 정보
-      </S.Tab>
     </S.Container>
   );
 }
