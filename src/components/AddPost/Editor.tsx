f726cb11040e6ee9839c18adf09e93cdc65295ed
jdw09 <razion0304@naver.com>
2025-10-27 20:29:55 +0900

    style: 주석 및 코드 가독성 향상

--- src/components/AddPost/Editor.tsx
+++ src/components/AddPost/Editor.tsx
@@ -2,7 +2,6 @@ import ReactQuill from "react-quill-new";
 import 'react-quill-new/dist/quill.snow.css'
 import { QuillStyle, QuillWrapper } from "./Editor.style";
 import { useRef } from "react";
-// import './editor.css'
 
 interface IProps {
     value?: string;
@@ -13,29 +12,25 @@ function Editor({value, setValue}:IProps) {
     const quillRef = useRef<ReactQuill>(null);
 
     const imageHandler = () => {
-        console.log("imagehanlder")
-        const input = document.createElement('input');
+        const input = document.createElement('input'); //가상의 input을 만들어 이미지를 업로드하도록 유도
         input.setAttribute('type', 'file');
         input.setAttribute('accept', 'image/*');
         input.click();
         input.addEventListener("change", async () => {
-            const file = input.files?.[0];
+            const file = input.files?.[0]; //업로드한 이미지 파일
             try {
-
-                console.log("in");
-                const name = Date.now();
-                //image upload func
+                const name = Date.now(); //업로드 할 이미지의 이름을 현재 시각으로
+                //TODO: image upload func
 
                 //placeholderImage
                 const imageUrl = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTpvRRub-ODLG5AAHLD4g1Hyx1X1RAbvUPuBpdM1whwQqdOOQiZzfSvuDtGrRgADTc-HMNwlumHxyAG4GFg7IKcs25oU0D2Qm9lWmwBpz0";
 
-                const editor = quillRef.current?.getEditor();
-
-                const range = editor?.getSelection();
-                editor?.insertEmbed(range?.index || 0, "image", imageUrl);
+                const editor = quillRef.current?.getEditor(); //에디터 가져오기
+                const range = editor?.getSelection(); //현재 커서 위치
+                editor?.insertEmbed(range?.index || 0, "image", imageUrl); //이미지 삽입
             }
             catch(error) {
-                console.log(error);
+                alert(error);
             }
         })
     }
@@ -49,7 +44,7 @@ function Editor({value, setValue}:IProps) {
             ['link', 'image'],
         ],
         handlers: {
-            image: imageHandler
+            image: imageHandler //이미지 업로드 시 imageHandler 사용
         }
     },
     };
