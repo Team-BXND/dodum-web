6fd9c1269f0e013c06d0bdb9c043b49e5fcbebc3
황경민 <aa01046044029@gmail.com>
2025-11-05 17:40:48 +0900

    대회정보 카드 구현

--- src/pages/competition_info/Competition_info.tsx
+++ src/pages/competition_info/Competition_info.tsx
@@ -4,9 +4,11 @@ const Competition_info = () => {
         <>
         <S.PageTitle>대회정보</S.PageTitle>
         <S.TitleLine></S.TitleLine>
-        <S.Card>
-            
-        </S.Card>
+        <S.Cardcover>
+            <S.Card_top></S.Card_top>
+            <S.Comp_card></S.Comp_card>
+        </S.Cardcover>
+
         </>
     )
 }
