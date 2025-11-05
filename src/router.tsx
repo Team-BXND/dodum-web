3fbd3f2a4edea0568d531f58b96f040a23cd3c52
황경민 <aa01046044029@gmail.com>
2025-11-05 17:51:13 +0900

    Revert "제목입력"

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,5 @@
 import { createBrowserRouter } from 'react-router';
 import Major from '@/pages/Major/Major';
-import Competition_info from './pages/competition_info/Competition_info';
-import Competition_info_add from './pages/competition_info/Competiton_info_add';
 import App from './App';
 
 const router = createBrowserRouter([
@@ -13,14 +11,6 @@ const router = createBrowserRouter([
         path: 'major',
         element: <Major />,
       },
-      {
-        path:"competition_info",
-        element:<Competition_info/>
-      },
-      {
-        path:"competition_info_add",
-        element:<Competition_info_add/>
-      }
     ],
   },
 ]);
