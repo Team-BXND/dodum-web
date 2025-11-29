97928ba1116b76269e1f1152fb48aa4e73f14735
jdw09 <razion0304@naver.com>
2025-11-29 16:31:26 +0900

    fix: 상세 페이지 진입 시 카테고리 숨김처리

--- src/pages/Archive/Archive.tsx
+++ src/pages/Archive/Archive.tsx
@@ -11,7 +11,7 @@ function Archive() {
     return (
         <S.Container>
             <PageTitle line text='아카이브' path={path.length == 4 ? path[2] : null}/>
-            {(path.includes("add") || path.includes("edit")) ? null : <Category categories={categories}/>}
+            {(path.includes("add") || path.includes("edit") || path.length === 4) ? null : <Category categories={categories}/>}
             <Outlet />
         </S.Container>
     );
